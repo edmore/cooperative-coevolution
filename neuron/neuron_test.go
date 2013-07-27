017e0c0780a92d1d76137bb759422a50c8d62920
Edmore Moyo <etmoyo@gmail.com>
2013-07-27 14:31:26 +0200

    edit test to make it pass

--- neuron/neuron_test.go
+++ neuron/neuron_test.go
@@ -3,7 +3,7 @@ package neuron
 import "testing"
 
 func TestNewNeuron(t *testing.T) {
-	const in, out = 5, 6
+	const in, out = 5, 5
 	var n = NewNeuron(in)
 	if x := len(n.weight); x != out {
 		t.Errorf("len(NewNeuron(%v).weight) = %v, we want %v", in, x, out)
