ffd02eb79758d919b652eee8fc50a9cb4aa16724
Edmore Moyo <etmoyo@gmail.com>
2013-08-28 16:51:58 +0200

    fix formating

--- client.go
+++ client.go
@@ -21,15 +21,14 @@ func main() {
 	}
 	var pong, ping string
 
-
-		// Arith Service
-		args := &service.Args{7, 8}
-		var reply int
-		err = client.Call("Arith.Mul", args, &reply)
-		if err != nil {
-			log.Fatal("arith error:", err)
-		}
-		fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
+	// Arith Service
+	args := &service.Args{7, 8}
+	var reply int
+	err = client.Call("Arith.Mul", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
 
 	// Ping Service
 	for ping != "exit" {
