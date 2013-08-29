abc910b30d52372ee2ca5b46bcd96d277c332a7c
Edmore Moyo <etmoyo@gmail.com>
2013-08-29 15:08:48 +0200

    adding flags for server

--- server.go
+++ server.go
@@ -6,9 +6,19 @@ import (
 	"net"
 	"net/http"
 	"net/rpc"
+	"flag"
+	"os"
 )
 
+var	port = flag.String("port", "", "server listening port; must be set.")
+
 func main() {
+	flag.Parse()
+	if *port == "" {
+		flag.Usage()
+		os.Exit(2)
+	}
+
 	log.Println("Server starting up ...")
 	rpc.Register(new(service.Arith))
 	log.Println("Arith Service Registered.")
@@ -16,7 +26,7 @@ func main() {
 	log.Println("Ping Service Registered.")
 	rpc.HandleHTTP()
 
-	l, e := net.Listen("tcp", ":1234")
+	l, e := net.Listen("tcp", ":" + *port)
 	log.Println("Server listening on", l.Addr())
 	if e != nil {
 		log.Fatal("listen error:", e)
