eba193bdbea643a4148787dd2e3206303f93c8a2
Edmore Moyo <etmoyo@gmail.com>
2013-10-15 13:55:12 +0200

    logically moving around variables and constants

--- cartpole/cartpole.go
+++ cartpole/cartpole.go
@@ -13,6 +13,20 @@ var (
 	thirtysixDegrees float64 = 0.628329
 	sixtyfourDegrees float64 = 1.2566580
 	fiftyDegrees     float64 = 0.87266
+	inputDimension   int     = 7
+	outputDimension  int     = 1
+	state                    = make([]float64, inputDimension)
+	tau              float64 = 0.01 //seconds between state updates (the time step)
+	euler_tau        float64 = tau / 4
+	maxFitness       int     = 1000
+	poleInc          float64 = 0.05
+	massInc          float64 = 0.01
+)
+
+const (
+	RK4     bool    = true
+	Gravity float64 = 9.8
+	MinInc  float64 = 0.001
 )
 
 type Cartpole struct {
@@ -22,15 +36,12 @@ type Cartpole struct {
 	TrackSize  float64
 	MUp        float64 // Pole-hinge Friction Coefficient
 	MUc        float64 // Cart-track Friction Coefficient
-	Gravity    float64
 	MassCart   float64
 	MassPole1  float64
 	MassPole2  float64
 	Length1    float64 // actually half the pole's length
 	Length2    float64
 	ForceMag   float64
-	TAU        float64 //seconds between state updates (the time step)
-
 }
 
 func NewCartpole(markov bool, numofpoles int) *Cartpole {
@@ -41,12 +52,10 @@ func NewCartpole(markov bool, numofpoles int) *Cartpole {
 		TrackSize:  2.4,
 		MUp:        0.000002,
 		MUc:        0.0005,
-		Gravity:    -9.8,
 		MassCart:   1.0,
 		MassPole1:  0.1,
 		MassPole2:  0.01,
 		Length1:    0.5,
 		Length2:    0.05,
-		ForceMag:   10.0,
-		TAU:        0.01}
+		ForceMag:   10.0}
 }
