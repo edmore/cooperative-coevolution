33eedf83696eb9d5a1a8e051869cd3f4a98e95fb
Edmore Moyo <etmoyo@gmail.com>
2013-11-04 23:41:12 +0200

    euler method implementation

--- rungekutta/euler/euler.go
+++ rungekutta/euler/euler.go
@@ -1,1 +1,32 @@
+/*
+ Package euler implements the Runge-Kutta First Order (Euler) Method
+*/
+
 package euler
+
+import "fmt"
+
+type Point struct {
+	X, Y float64
+}
+
+type Equation func(float64) float64
+
+// Initialise a new point
+func NewPoint(x float64, y float64) *Point {
+	return &Point{X: x, Y: y}
+}
+
+// Step
+func step(dx float64, dydx Equation, p *Point) {
+	dy := dx * dydx(p.X)
+	p.X, p.Y = p.X+dx, p.Y+dy
+}
+
+// Solve for y = f(x)
+func (p *Point) Solve(dx float64, dydx Equation, xFinal float64) float64 {
+	for p.X < xFinal {
+		step(dx, dydx, p)
+	}
+	return p.Y
+}
---
+++ rungekutta/euler/euler_test.go
@@ -0,0 +1,26 @@
+package euler
+
+import "testing"
+
+func TestNewPoint(t *testing.T) {
+	const x, y = 1, 0
+	p := NewPoint(x, y)
+	if p.X != 1 {
+		t.Errorf("value of x should be %v, we got %v", x, p.X)
+	}
+	if p.Y != 0 {
+		t.Errorf("value of x should be %v, we got %v", y, p.Y)
+	}
+}
+
+func TestSolve(t *testing.T) {
+	eq := func(x float64) float64 { return 2 * (x - 1) }
+	const x, y, dx, xFinal, yFinal = 1, 0, 0.5, 3, 3
+	p := NewPoint(x, y)
+	solution := p.Solve(dx, eq, xFinal)
+
+	if solution != yFinal {
+		t.Errorf("value of yFinal should be %v, we got %v", yFinal, solution)
+	}
+
+}
