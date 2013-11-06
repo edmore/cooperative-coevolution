927234d0d759a1e4c95c9f3df6f9456271f6b745
Edmore Moyo <etmoyo@gmail.com>
2013-11-06 17:26:56 +0200

    classic runge-kutta method

--- rungekutta/classic/classic.go
+++ rungekutta/classic/classic.go
@@ -1,1 +1,47 @@
 package classic
+
+/*
+ Package classic implements the Runge-Kutta Fourth Order (Rk4) Method
+*/
+
+/*********************************
+
+dx - step size / change in x
+dydx - the differential equation
+
+**********************************/
+
+type Point struct {
+	X, Y float64
+}
+
+type Equation func(*Point) float64
+
+// Initialise a new point
+func NewPoint(x float64, y float64) *Point {
+	return &Point{X: x, Y: y}
+}
+
+// Step
+func step(dx float64, dydx Equation, p *Point) {
+	temp := new(Point)
+	temp.X, temp.Y = p.X, p.Y
+
+	k1 := dydx(temp)
+	temp.X, temp.Y = p.X+(0.5*dx), p.Y+(0.5*k1*dx)
+	k2 := dydx(temp)
+	temp.X, temp.Y = p.X+(0.5*dx), p.Y+(0.5*k2*dx)
+	k3 := dydx(temp)
+	temp.X, temp.Y = p.X+dx, p.Y+k3*dx
+	k4 := dydx(temp)
+	p.Y = p.Y + (1.0/6.0)*(k1+2*k2+2*k3+k4)*dx
+	p.X = temp.X
+}
+
+// Solve for y = f(x)
+func (p *Point) Solve(dx float64, dydx Equation, xFinal float64) float64 {
+	for p.X < xFinal {
+		step(dx, dydx, p)
+	}
+	return p.Y
+}
---
+++ rungekutta/classic/classic_test.go
@@ -0,0 +1,31 @@
+package classic
+
+import (
+	"math"
+	"testing"
+)
+
+func TestNewPoint(t *testing.T) {
+	const x, y = 1, 0
+	p := NewPoint(x, y)
+	if p.X != 1 {
+		t.Errorf("value of x should be %v, we got %v", x, p.X)
+	}
+	if p.Y != 0 {
+		t.Errorf("value of x should be %v, we got %v", y, p.Y)
+	}
+}
+
+func TestSolve(t *testing.T) {
+	eq := func(p *Point) float64 {
+		return -2.2067 * math.Pow(10, -12) * (math.Pow(p.Y, 4) - 81*math.Pow(10, 8))
+	}
+	const x, y, dx, xFinal, yFinal = 0, 1200, 240, 480, 594.912631110278
+	p := NewPoint(x, y)
+	solution := p.Solve(dx, eq, xFinal)
+
+	if solution != yFinal {
+		t.Errorf("value of yFinal should be %v, we got %v", yFinal, solution)
+	}
+
+}
