51a5b61bca3f706a8292e01172794d99dea169c7
Edmore Moyo <etmoyo@gmail.com>
2013-11-30 01:33:48 +0200

    cart pole functions and tests

--- environment/cartpole/cartpole.go
+++ environment/cartpole/cartpole.go
@@ -1,61 +1,78 @@
 /*
- Package environment implements the simulation environment(s)
- Current implementation - Pole balancing / Inverted Pendulum Task (double pole balancing with full state information)
+ Package cartpole implements the Double Pole balancing / Inverted Pendulum Task.
+ Currently - with full state information (markov)
 */
 
 package cartpole
 
+import "math"
+
 var (
-	bias             float32 = 0.5
-	oneDegree        float64 = 0.0174532 // 2pi/360
-	sixDegrees       float64 = 0.1047192
-	twelveDegrees    float64 = 0.2094384
-	fifteenDegrees   float64 = 0.2617993
-	thirtysixDegrees float64 = 0.628329
-	sixtyfourDegrees float64 = 1.2566580
-	fiftyDegrees     float64 = 0.87266
-	inputDimension   int     = 7
-	outputDimension  int     = 1
-	tau              float64 = 0.01 //seconds between state updates (the time step)
-	euler_tau        float64 = tau / 4
-	maxFitness       int     = 1000
-	poleInc          float64 = 0.05
-	massInc          float64 = 0.01
-	minInc           float64 = 0.001
-	gravity          float64 = 9.8
-	RK4              bool    = true
-	state                    = make([]float64, inputDimension-1)
-	input                    = make([]float64, inputDimension)
-	output                   = make([]float64, outputDimension)
+	ForceMag float64 = 10.0 // Force magnitude - (+ / -) 10.0
+	Tau      float64 = 0.01 //seconds between state updates (the time step)
 )
 
-type Cartpole struct {
-	Name       string
-	NumOfPoles int
-	Markov     bool
-	TrackSize  float64
-	MUp        float64 // Pole-hinge Friction Coefficient
-	MUc        float64 // Cart-track Friction Coefficient
-	MassCart   float64
-	MassPole1  float64
-	MassPole2  float64
-	Length1    float64 // actually half the pole's length
-	Length2    float64
-	ForceMag   float64 // force magnitude in Newtons
+const (
+	RadToDeg         = 180 / math.Pi
+	DegToRad         = math.Pi / 180
+	Gravity  float64 = 9.81
+)
+
+type State struct {
+	X         float64 // position of the cart
+	XDot      float64 // velocity of the cart
+	Theta1    float64 // angle of the 1st pole
+	ThetaDot1 float64 // angular velocity of the 1st pole
+	Theta2    float64 // angle of the 2nd pole
+	ThetaDot2 float64 // angular velocity of the 2nd pole
 }
 
-func NewCartpole(markov bool, numofpoles int) *Cartpole {
+var state *State = new(State)
+
+type Cartpole struct {
+	Name      string
+	TrackSize float64
+	Up        float64 // Pole-hinge Friction Coefficient
+	Uc        float64 // Cart-track Friction Coefficient
+	MassCart  float64
+	MassPole1 float64
+	MassPole2 float64
+	Length1   float64 // actually half the pole's length
+	Length2   float64 // actually half the pole's length
+}
+
+func NewCartpole() *Cartpole {
 	return &Cartpole{
-		Name:       "Pole Balancing Task",
-		NumOfPoles: numofpoles,
-		Markov:     markov,
-		TrackSize:  2.4,
-		MUp:        0.000002,
-		MUc:        0.0005,
-		MassCart:   1.0,
-		MassPole1:  0.1,
-		MassPole2:  0.01,
-		Length1:    0.5,
-		Length2:    0.05,
-		ForceMag:   10.0}
+		Name:      "Double Pole Balancing Task",
+		TrackSize: 2.4,
+		Up:        0.000002,
+		Uc:        0.0005,
+		MassCart:  1.0,
+		MassPole1: 0.1,
+		MassPole2: 0.01,
+		Length1:   0.5,
+		Length2:   0.05}
+}
+
+// Re-initialize the environment
+func (c *Cartpole) Reset() *State {
+	state.X = 0.2
+	state.Theta1 = DegToRad
+	return state
+}
+
+// Stores the desired action for the next Runge-Kutta step
+func (c *Cartpole) PerformAction(action int) *State {
+	state := step(action)
+	return state
+}
+
+// Runge-Kutta Step - approximate state variables at time dt
+func step(action int) *State {
+	if action == 0 {
+		F := ForceMag * -1
+	} else {
+		F := ForceMag
+	}
+	return state
 }
--- environment/cartpole/cartpole_test.go
+++ environment/cartpole/cartpole_test.go
@@ -1,1 +1,20 @@
 package cartpole
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewCartpole(t *testing.T) {
+	c := NewCartpole()
+	fmt.Println(c)
+}
+
+func TestReset(t *testing.T) {
+	c := NewCartpole()
+	s := c.Reset()
+	fmt.Println(s)
+}
+
+func TestPerformAction(t *testing.T) {
+}
