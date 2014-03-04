b7389c6383e086a53a65bcd07894007f313c53fc
Edmore Moyo <etmoyo@gmail.com>
2014-03-04 22:38:17 +0200

    evaluation code not in use can be ignored from builds

---
+++ x_evaluate.go
@@ -0,0 +1,27 @@
+// +build ignore
+
+package main
+
+import (
+	"github.com/edmore/esp/environment"
+	"github.com/edmore/esp/network"
+	"github.com/edmore/esp/neuron"
+)
+
+var ch = make(chan []*neuron.Neuron)
+
+// Evaluate the network in the trial environment
+func evaluate(e environment.Environment, n network.Network) {
+	// loop while within bounds
+	// e.PerformAction(n.Activate())
+	// fitness++
+
+	// award fitness score to network
+	// define setter method for network fitness
+
+	// add the fitness score to cumulative fitness of
+	// neurons that participated in trial.
+	// beware of race conditions when adding the fitness
+	// to each neuron needs to be synchronized.
+	ch <- n.GetHiddenUnits()
+}
